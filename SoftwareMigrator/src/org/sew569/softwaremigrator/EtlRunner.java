1e3d6e7afd791ed6e2ad9825627786b62dbc6968
sofaw <soph.wood52@gmail.com>
2018-07-10 10:21:36 +0100

    reformat etlrunner

--- SoftwareMigrator/src/org/sew569/softwaremigrator/EtlRunner.java
+++ SoftwareMigrator/src/org/sew569/softwaremigrator/EtlRunner.java
@@ -1,7 +1,5 @@
 package org.sew569.softwaremigrator;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +9,15 @@ import org.eclipse.epsilon.eol.execute.context.Variable;
 import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.epsilon.etl.EtlModule;
 
-// TODO: format and remove unused fields/methods
 public class EtlRunner {
 	private String inputXml;
 	private String outputModel;
 	private String configXml;
+
+	protected EtlModule module;
+	protected List<Variable> parameters = new ArrayList<Variable>();
+
+	protected Object result;
 
 	public EtlRunner(String inputXml, String outputModel, String configXml) {
 		this.inputXml = inputXml;
@@ -27,33 +29,8 @@ public class EtlRunner {
 		return new EtlModule();
 	}
 
-	private boolean createOutputModel(String path) throws IOException {
-		File f = new File(path);
-		f.getParentFile().mkdirs();
-		return f.createNewFile();
-	}
-
 	public List<IModel> getModels() throws Exception {
 		List<IModel> models = new ArrayList<IModel>();
-		// Create model output file if it doesn't exist
-		// createOutputModel("src/resources/transform_output/out.model");
-
-		// TODO: why can't classloader find this file?
-		// Seems to work when call refresh in Eclipse
-		/*
-		 * ResourcesPlugin.getWorkspace().getRoot().getProjects(); IFile iFile =
-		 * ResourcesPlugin.getWorkspace().getRoot().getFile(new
-		 * Path("src/resources/transform_output/out.model"));
-		 * iFile.refreshLocal(IResource.DEPTH_ZERO, null); ClassLoader c =
-		 * ModelLoader.class.getClassLoader(); System.out.println(c.getResource(
-		 * "resources/transform_output/out.model") == null);
-		 * System.out.println(Helper.getFileURI(
-		 * "resources/transform_output/out.model") == null);
-		 */
-		/*
-		 * ClassLoader c = ModelLoader.class.getClassLoader();
-		 * System.out.println(c.getResource(outputModel) == null);
-		 */
 		models.add(
 				ModelLoader.createEmfModel("OUT", outputModel, "resources/metamodel/metamodel_v2.ecore", false, true));
 		models.add(ModelLoader.createXmlModel("CONFIG", configXml, true, true));
@@ -65,11 +42,6 @@ public class EtlRunner {
 	public String getSource() throws Exception {
 		return "resources/xml_to_model/xml_to_model.etl";
 	}
-
-	protected EtlModule module;
-	protected List<Variable> parameters = new ArrayList<Variable>();
-
-	protected Object result;
 
 	public void preProcess() {
 	};
